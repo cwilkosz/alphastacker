bca3b09a2b03566ce00c8c3f10e1014c19834de9
C Wilkosz <theewilkyway@gmail.com>
2017-02-11 10:40:53 -0600

    addedSpace

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -11,25 +11,56 @@
 
 // Invoke the function and pass in the array
 //stackLetters(alphabet)
-
 //var alphabet = [];
+//add 2nd string to put space on count 3
+
 var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var stackLetters = "";
 
 // The stackLetter function should accept the array as the sole argument
-function stackLetters (theAlphabetArray) {
-
-}
-   for (var i =0; i<alphabet.length; i++){
-    stackLetters += alphabet[i]
-  	console.log( stackLetters += alphabet[i]) ;
-//   alphabet[i];
-//	console.log("alphabet", alphabet)
-//	console.log("stackLetters", stackLetters);
-}
 //function stackLetters (theAlphabetArray) {
 
 //}
 
-//stackLetters(alphabet)
-   
+for (var i =0; i<alphabet.length; i++){
+    stackLetters += alphabet[i]
+  	console.log( stackLetters) ;
+  	if ((i + 1) % 3 === 0){
+  		stackLetters +=" ";
+
+  	}
+
+}
+	
+	
+
+
+
+//	console.log( stackLetters += alphabet[i]) ;
+
+//   alphabet[i];
+//	console.log("alphabet", alphabet)
+//	console.log("stackLetters", stackLetters);
+
+//function stackLetters (theAlphabetArray) {
+
+//}
+
+//var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//var stackLetters = "";
+
+// The stackLetter function should accept the array as the sole argument
+//function stackLetters (theAlphabetArray) {
+
+// }
+
+// for (var i =0; i<alphabet.length; i++){
+//     stackLetters += alphabet[i]
+  	
+// }
+
+// 	if ((i + 1) % 3 === 0){
+
+// 	}
+// 	console.log( stackLetters) ;
+
